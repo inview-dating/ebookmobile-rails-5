99e4cec491b5bc4ed7ec9f0e739ee2c0a8d45555
Dana Scheider <dana.scheider@gmail.com>
2016-07-14 18:04:46 -0700

    Fix status code

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -18,7 +18,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.save
-      render json: @author, status: :created
+      render json: @author, status: :ok
     else
       render json: @author.errors, status: :unprocessable_entity
     end
