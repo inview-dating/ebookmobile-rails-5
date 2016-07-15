80fb81ad9ae0ab62467dbffc421d94083cb1b39a
Dana Scheider <dana.scheider@gmail.com>
2016-07-14 18:03:54 -0700

    A bunch of stuff

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -18,7 +18,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
 
     if @author.save
-      render json: @author, status: :created, location: @author
+      render json: @author, status: :created
     else
       render json: @author.errors, status: :unprocessable_entity
     end
